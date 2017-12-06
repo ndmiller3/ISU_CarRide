1039ade509186d46490340eb3320fbad601fcf54
Daniel Tidyman <fendersfolly@icloud.com>
2017-12-05 22:16:25 -0600

    Updated Menu and Stats
    Made DailyStatsWrite to file and updated the menu with error corrections

--- src/ProjectLogic/MainRun.java
+++ src/ProjectLogic/MainRun.java
@@ -25,17 +25,25 @@ public class MainRun {
                   "* Welcome to ISU's Car Ride System! *\n" +
                   "*                                   *" +
                   "\n**************************************");
-          System.out.println("\nPlease select: \n(1) I am a new Customer " +
+          String menuOptions = "\nPlease select: \n(1) I am a new Customer " +
                   "\n(2) I am a new Driver \n" +
                   "(3) I am a Previous customer that needs to login and call a ride\n" +
                   "(4) I am a Previous driver that needs to login" +
                   "\n(5) Run Daily Reports\n" +
-                  "(6) Exit the program");
-          System.out.println("\nPlease type number of selection here: ");
+                  "(6) Exit the program";
 
-        while(running){
+        do{
+            System.out.println(menuOptions);
+            System.out.println("\nPlease type number of selection here: ");
 
-          switch(mainMenu.nextInt())
+            while(!mainMenu.hasNextInt()){
+                mainMenu.next();
+                System.out.println(menuOptions);
+                System.out.println("\nPlease type number of selection here: ");
+            }
+            int choicemainMenu=mainMenu.nextInt();
+
+          switch(choicemainMenu)
           {
               //creates a new Rider for the database and calls a ride
               case 1:
@@ -43,6 +51,7 @@ public class MainRun {
                   rider.NewCustomer();
                   System.out.println("Welcome to the ISU Car Ride System!");
                   rider.callRide();
+                  break;
 
                   //creates a 4new Driver for the database and gets a default ride
               case 2:
@@ -59,6 +68,7 @@ public class MainRun {
                   rider = new Rider();
                   rider.customerLogin();
                   rider.callRide();
+                  break;
 
                   //logs in a new driver and calls a default ride
               case 4:
@@ -80,11 +90,20 @@ public class MainRun {
               case 5:
                   DailyReport dailyReport = new DailyReport();
                   dailyReport.displayReport();
+                  mainMenu.next();
+                  break;
 
               case 6:
                   running = false;
+                  break;
+
+              default:
+                  mainMenu.next();
+                  break;
+
+
           }
-      }
+      }while(running);
     }
 
 
